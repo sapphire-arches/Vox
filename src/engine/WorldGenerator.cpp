9366c4aca6ddfc10a806c1851b7f4ffa1e9477ed
bobtwinkles <srkoser+GitHub@gmail.com>
2013-01-15 13:44:01 -0500

    Made world generation more interesting.

--- src/engine/WorldGenerator.cpp
+++ src/engine/WorldGenerator.cpp
@@ -1,5 +1,7 @@
 #include "WorldGenerator.hpp"
+#include <cmath>
 
+using namespace std;
 using namespace vox::engine;
 
 WorldGenerator::WorldGenerator(int Seed) :
@@ -19,14 +21,20 @@ WorldGenerator& WorldGenerator::operator= (const WorldGenerator& Other) {
     return *this;
 }
 
+#define MAX_WORLD_SIZE 128
+
 int WorldGenerator::GetBlock(int X, int Y, int Z) {
-    float fac =  Y * -0.05f;
-    if (fac < -1)
-        return 0;
-    if (fac > 0)
+    if (
+            abs(X) > MAX_WORLD_SIZE ||
+            abs(Y) > MAX_WORLD_SIZE ||
+            abs(Z) > MAX_WORLD_SIZE) {
         return 1;
+    }
+
+    float fac = 0.;
     fac += _per.Get(X * 0.05f, Y * 0.05f, Z * 0.05f);
-    if (fac > 0)
+    if (fac > 0.5f) {
         return 1;
+    }
     return 0;
 }
