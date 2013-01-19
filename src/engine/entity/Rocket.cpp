572661d3d8b68f81fd1478794ae3244beca2a7ff
bobtwinkles <srkoser+GitHub@gmail.com>
2013-01-19 16:46:26 -0500

    Rocket destroys radius.

--- src/engine/entity/Rocket.cpp
+++ src/engine/entity/Rocket.cpp
@@ -12,6 +12,8 @@ Rocket::Rocket(vec3 Pos, vec3 Dir, const PhysicsObject& Parent) : Entity(Pos, ve
     _detonate = false;
 }
 
+#define SPLOSION_RADIUS 5
+
 void Rocket::Tick(vox::engine::World& W) {
     DoPhysics(W);
     ApplyForce((_dir * 3.f) - _vel);
@@ -22,7 +24,15 @@ void Rocket::Tick(vox::engine::World& W) {
         int x = vox::Floor(_aabb.X + _dir.x);
         int y = vox::Floor(_aabb.Y + _dir.y);
         int z = vox::Floor(_aabb.Z + _dir.z);
-        W.SetBlock(x, y, z, 0);
+        for (int xx = -SPLOSION_RADIUS; xx <= SPLOSION_RADIUS; ++xx) {
+            for (int yy = -SPLOSION_RADIUS; yy <= SPLOSION_RADIUS; ++yy) {
+                for (int zz = -SPLOSION_RADIUS; zz <= SPLOSION_RADIUS; ++zz) {
+                    if (xx * xx + yy * yy + zz * zz < SPLOSION_RADIUS * SPLOSION_RADIUS) {
+                        W.SetBlock(x + xx, y + yy, z + zz, 0);
+                    }
+                }
+            }
+        }
     }
 }
 
